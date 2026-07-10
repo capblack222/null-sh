7f69e57d555eb2325af8cbd63eb0d06a6d55a250
CapBlack222 <nish.gup.446@gmail.com>
2026-07-09 23:03:11 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import sys
 
+built = ['exit', 'type', 'echo']
 
 def main():
     while True:
@@ -8,6 +9,11 @@ def main():
         if input_command[0] != "exit":
             if input_command[0] == "echo":
                 print(" ".join(input_command[1:]))
+            elif input_command[0] == "type":
+                if input_command[1] in built:
+                    print(f"{input_command[1]} is a shell builtin")
+                else:
+                    print(f"{input_command[1]}: not found")
             else:
                 print(f"{input_command[0]}: command not found")
         else:
