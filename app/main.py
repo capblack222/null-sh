da6efc6f39868276302d69152de290deed4e69fc
CapBlack222 <nish.gup.446@gmail.com>
2026-07-09 22:59:27 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,9 +4,12 @@ import sys
 def main():
     while True:
         sys.stdout.write("$ ")
-        input_command = sys.stdin.readline().split()[0]
-        if input_command != "exit":
-            sys.stdout.write(f"{input_command}: command not found\n")
+        input_command = sys.stdin.readline().split()
+        if input_command[0] != "exit":
+            if input_command[0] == "echo":
+                print(" ".join(input_command[1:]))
+            else:
+                print(f"{input_command[0]}: command not found")
         else:
             break
     pass
