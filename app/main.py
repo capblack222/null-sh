dbe97ef89b2511b22fd7d821c5ed62e0cce3f49f
CapBlack222 <nish.gup.446@gmail.com>
2026-07-09 22:46:29 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import sys
 
 def main():
     sys.stdout.write("$ ")
-    input_command = sys.stdin.read()
+    input_command = sys.stdin.readline().split()[0]
     sys.stdout.write(f"{input_command}: command not found\n")
     sys.stdout.write("$ ")
     pass
