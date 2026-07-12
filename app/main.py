2bb2daf8881dc353115467d72bf926eae9e8d51f
CapBlack222 <nish.gup.446@gmail.com>
2026-07-11 22:02:00 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import sys
 import shutil
 import subprocess
+from pathlib import Path
 
 built = ['exit', 'type', 'echo']
 
@@ -12,14 +13,18 @@ def main():
             if input_command[0] == "echo":
                 print(" ".join(input_command[1:]))
             elif input_command[0] == "type":
-                if input_command[1] in built:
-                    print(f"{input_command[1]} is a shell builtin")
+                if len(input_command) == 1:
+                    print("Nothing was passed")
                 elif shutil.which(input_command[1]):
                     print(f"{input_command[1]} is {shutil.which(input_command[1])}")
+                elif input_command[1] in built:
+                    print(f"{input_command[1]} is a shell builtin")
                 else:
                     print(f"{input_command[1]}: not found")
             elif shutil.which(input_command[0]):
                 subprocess.run(input_command)
+            elif input_command[0] == "pwd":
+                print(Path.cwd())
             else:
                 print(f"{input_command[0]}: command not found")
         else:
